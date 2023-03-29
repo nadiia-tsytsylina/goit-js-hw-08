fe57ba45dd9a9cb2be3957b62576221aa8c5cf06
Nadiia Tsytsylina <miniova95@gmail.com>
2023-03-29 15:03:53 +0300

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+function createLi(array) {
+  return array.reduce(
+    (acc, item) =>
+      acc +
+      `<li class="gallery__item">
+          <a class="gallery__link" href="${item.original}">
+            <img
+              class="gallery__image"
+              src="${item.preview}"
+              alt="${item.description}"
+            />
+          </a>
+        </li>`,
+    ''
+  );
+}
+
+const galleryUl = document.querySelector('.gallery');
+galleryUl.insertAdjacentHTML('beforeend', createLi(galleryItems));
+galleryUl.style.listStyle = 'none';
+
+const images = document.querySelectorAll('.gallery__image');
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
