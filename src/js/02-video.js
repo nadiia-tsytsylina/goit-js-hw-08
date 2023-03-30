3f80259a3ee78c48f076743e8382a42743ea9e4f
Nadiia Tsytsylina <miniova95@gmail.com>
2023-03-30 15:38:31 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import Player from '@vimeo/player';
+let throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(function (currentTime) {
+    localStorage.setItem('videoplayer-current-time', currentTime.seconds);
+  }, 1000)
+);
+
+const time = localStorage.getItem('videoplayer-current-time');
+
+player
+  .setCurrentTime(time)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
